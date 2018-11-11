d60fe3cad20e8e39af068fca43b233d44687cfc6
Christopher Carr <38837941+ChrisKCarr@users.noreply.github.com>
2018-11-11 17:27:58 -0500

    Update Website_blocker.py

--- Website_blocker.py
+++ Website_blocker.py
@@ -7,7 +7,7 @@ redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com","www.Youtube.com","Youtube.com"]
 
 while True:
-    if dt(dt.now().year),dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+    if dt(dt.now().year),dt.now().month,dt.now().day,6) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,22):
         print("Working Hours..")
         with open(hosts_temp, 'r') as file:
             content=file.read()
