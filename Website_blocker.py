bc4375d21cec534815de2514e75f6bc069aa363b
Christopher Carr <38837941+ChrisKCarr@users.noreply.github.com>
2018-11-12 16:09:48 -0500

    Update Website_blocker.py

--- Website_blocker.py
+++ Website_blocker.py
@@ -1,29 +1,29 @@
 import time
 from datetime import datetime as dt
 
-hosts_temp=r"D:\Dropbox\pp\block_websites\Demo\hosts"
-hosts_path=r"C:\Users\carrc\Desktop\CODEING\Complete-Python-Bootcamp-master\Website_Blocker\Hosts"
+hosts_temp="hosts" 
+hosts_path=r"C:\Windows\System32\drivers\etc\hosts" #This should be the path to your hosts file on your OS. 
 redirect="127.0.0.1"
-website_list=["www.facebook.com","facebook.com","www.Youtube.com","Youtube.com"]
+website_list=["ENTER IN WEBSITE HERE"]
 
 while True:
-    if dt(dt.now().year),dt.now().month,dt.now().day,6) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,22):
-        print("Working Hours..")
-        with open(hosts_temp, 'r') as file:
+    if dt(dt.now().year,dt.now().month,dt.now().day,6) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,22):
+        #Enter in the times you want the websites to be blocked above, and when they should be unblocked. 
+        print("Busy hours..")
+        with open(hosts_path, 'r+') as file:
             content=file.read()
-            print(content)
             for website in website_list:
                 if website in content:
                     pass
                 else:
                     file.write(redirect+" "+website+"\n")
     else:
-        with open(hosts_temp,'r+') as file:
-            content=file.readline()
+        with open(hosts_path, 'r+') as file:
+            content=file.readlines()
             file.seek(0)
             for line in content:
                 if not any(website in line for website in website_list):
                     file.write(line)
             file.truncate()
-        print("Free Hours..")
+        print("Free time..")
     time.sleep(5)
